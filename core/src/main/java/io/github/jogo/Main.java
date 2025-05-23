67ca084eb6a7480a238eb13072448c596f972dde
marcelo <20241915@iade.pt>
2025-05-23 09:45:45 +0100

    Main clean dependencies

--- core/src/main/java/io/github/jogo/Main.java
+++ core/src/main/java/io/github/jogo/Main.java
@@ -3,8 +3,6 @@ package io.github.jogo;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import io.github.jogo.Scenes.*;
 import io.github.jogo.Screens.*;
 
